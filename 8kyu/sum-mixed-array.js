f6e4b31632c524ee9d32620c48cd5de965a60079
Abraham <aimejia4@gmail.com>
2022-08-18 23:46:02 -0500

    challenge added

--- 8kyu/sum-mixed-array.js
+++ 8kyu/sum-mixed-array.js
@@ -1,4 +1,10 @@
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return your answer as a number.
+// Return your answer as a number.
 
+
+//****** My Solution *****/
+
+function sumMix(x){
+    return x.map(num => +num).reduce((add, num) => add + num);
+  }
