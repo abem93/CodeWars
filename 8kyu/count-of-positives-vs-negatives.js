fbd2b6fe5d8e3566be5471de85c289473b3fa186
Abraham <aimejia4@gmail.com>
2022-08-30 00:29:06 -0500

    challenge added

--- 8kyu/count-of-positives-vs-negatives.js
+++ 8kyu/count-of-positives-vs-negatives.js
@@ -7,3 +7,19 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
+function countPositivesSumNegatives(input) {
+    let pos = 0
+    let neg = 0
+    if(!Array.isArray(input) || !input.length){
+      return []
+    }else{
+       input.forEach(x => {
+            if(x > 0){
+             pos += 1;
+            }else{
+              neg -= -x
+            }
+        })
+      return [pos,neg]
+     }
+  }
