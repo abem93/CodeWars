5112f251e3d97fee14dcb6df3a0a1aa9276b000d
Abraham <aimejia4@gmail.com>
2022-09-01 05:58:56 -0500

    New Challenge Added

---
+++ 8kyu/reduce-but-grow.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/57f780909f7e8e3183000078/
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+//********** My Solution *********//
+
+function grow(x){
+    return x.reduce((total, current) => total * current, 1)
+    }
