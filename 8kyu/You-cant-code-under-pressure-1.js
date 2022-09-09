89a864cc4ab1e2d37eac80047d472a69ada04d13
Abraham <aimejia4@gmail.com>
2022-09-08 23:12:37 -0500

    challenge added

---
+++ 8kyu/You-cant-code-under-pressure-1.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+//*************** My Solution ******************//
+
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i*2;
+  }
