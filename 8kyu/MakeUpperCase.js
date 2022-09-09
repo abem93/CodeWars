bd1a536baf1ebe5aa2465f515717489e66c92ecf
Abraham <aimejia4@gmail.com>
2022-09-08 23:10:58 -0500

    New Challenge Completed

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/javascript
+
+// Write a function which converts the input string to uppercase.
+
+//************** My Solution *****************//
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
