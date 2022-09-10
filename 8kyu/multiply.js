e90b114b1788fa0e861dfbf9656fa82754a17e3f
Abraham <aimejia4@gmail.com>
2022-09-10 09:17:42 -0500

    Challenge Completed

---
+++ 8kyu/multiply.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/50654ddff44f800200000004
+
+DESCRIPTION:
+This code does not execute properly. Try to figure out why.
+
+
+//*********** My Solution *************//
+
+function multiply(a, b){
+    return a * b
+   }
