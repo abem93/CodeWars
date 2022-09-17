7ef121482ca41811002a42ac08e2762aa95f950c
Abraham <aimejia4@gmail.com>
2022-09-16 22:19:46 -0500

    Challenge Completed

--- 8kyu/Mumbling.js
+++ 8kyu/Mumbling.js
@@ -10,4 +10,6 @@
 
 
 //************ My Solution *************//
- const function
+function accum(s) {
+    return s.split('').map((x,index) => x.toUpperCase()+Array(index+1).join(x.toLowerCase())).join('-');
+  }
