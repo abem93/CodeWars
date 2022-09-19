3166a59962fb361dcc43c47c3f6eb9e2fb09320d
Abraham <aimejia4@gmail.com>
2022-09-19 00:07:04 -0500

    New Challenge Completed

--- 8kyu/isograms.js
+++ 8kyu/isograms.js
@@ -10,3 +10,12 @@
 
 
 //**************** My Solution *********************//
+
+
+function isIsogram(str){
+    str = str.toLowerCase().split("").sort();
+    for (let i=0;i<str.length;i++){
+      if (str[i]==str[i+1]) return false;
+      }
+    return true;
+  }
