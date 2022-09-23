97682d9717eaf9e01ed5807500062d68295baf29
Abraham <aimejia4@gmail.com>
2022-09-23 03:21:30 -0500

    New Challenge Completed Two answers

--- 7kyu/remove-the-minimum.js
+++ 7kyu/remove-the-minimum.js
@@ -26,3 +26,10 @@ function removeSmallest(numbers) {
       return []
     }
   }
+
+function removeSmallest(numbers) {
+    numbers = numbers.slice(0)
+    const min = Math.min(...numbers);
+    numbers.splice(numbers.indexOf(min), 1);
+    return numbers;
+  }
