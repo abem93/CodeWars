7d237514e2bceb738522bbaa8595074e71b497b5
Abraham <aimejia4@gmail.com>
2022-09-29 03:42:21 -0500

    New Challenge Completed

--- 6kyu/multiple-of-3-or-5.js
+++ 6kyu/multiple-of-3-or-5.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+ https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
