958eaca4879efbe0bcd9a1f79d200ec4dbb3db9d
Abraham <aimejia4@gmail.com>
2022-09-30 02:02:30 -0500

    New Challenge Completed

---
+++ 7kyu/function-that-does-arithmetic.js
@@ -0,0 +1,16 @@
+https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+
+
+//************ My Solution ************//
+
+function arithmetic(a, b, operator){
+    if(operator == 'add'){
+      return a + b
+    }else if(operator == 'subtract'){
+      return a - b
+    }else if(operator == 'multiply'){
+      return a * b
+    }else if(operator == 'divide'){
+      return a / b
+    }
+  }
