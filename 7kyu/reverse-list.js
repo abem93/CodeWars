d66a38ecce2c9014f6143a12a4c30aa532af3dbc
Abraham <aimejia4@gmail.com>
2022-09-29 23:34:46 -0500

    New Challenge Completed

---
+++ 7kyu/reverse-list.js
@@ -0,0 +1,20 @@
+https://www.codewars.com/kata/57a04da9e298a7ee43000111/javascript
+
+// DESCRIPTION:
+// Write reverseList function that simply reverses lists.
+
+
+function reverseList(arr) {
+    return arr.reverse();
+  }
+
+  /*
+function reverseList(arr) {
+    const reversedArr = [];
+        for(let i = arr.length - 1; i >= 0; i--)
+        reverseArray.push(arr[i]);
+        return reversedArr;
+  }
+
+  */
+ 
