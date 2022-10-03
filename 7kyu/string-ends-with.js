3caabe67e88e3770f49b4434a0a625b5f503dbd3
Abraham <aimejia4@gmail.com>
2022-10-03 04:53:03 -0500

    Challenge Completed

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,16 @@
+https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/javascript
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+//*************** My Solution *****************//
+
+function solution(str, ending){
+    if(ending === ''){
+      return true
+    }else{
+    return str.slice(-ending.length) === ending
+  }}
