a15a12affd0ead4d2b32ad7eab04cebd402b87dd
Abraham Mejia <aimejia4@gmail.com>
2023-09-13 01:29:37 -0500

    New Challenges Completed

---
+++ 8kyu/the-if-function.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/54147087d5c2ebe4f1000805/javascript
+
+// DESCRIPTION:
+// Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+
+// When bool is truthy, func1 should be called, otherwise call the func2.
+
+// Example:
+// _if(true, function(){console.log("True")}, function(){console.log("false")})
+// // Logs 'True' to the console.
+
+
+// **************************************************
+
+function _if(bool, func1, func2) {
+    if(bool == true){
+      func1()
+    }else{
+    func2()
+      }
+}
